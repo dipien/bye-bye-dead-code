622d0040eb4198649d3cd3b3282c559036024922
Maxi Rosson <maxirosson@gmail.com>
2021-04-19 17:10:42 -0300

    ProjectExtensions fix

--- unused-android-code-gradle-plugin/src/main/java/com/jdroid/gradle/unusedandroidcode/commons/ProjectExtensions.kt
+++ unused-android-code-gradle-plugin/src/main/java/com/jdroid/gradle/unusedandroidcode/commons/ProjectExtensions.kt
@@ -2,7 +2,5 @@ package com.jdroid.gradle.unusedandroidcode.commons
 
 import org.gradle.api.Project
 
-private val propertyResolverCache = mutableMapOf<Project, PropertyResolver>()
-
 val Project.propertyResolver: PropertyResolver
-    get() = propertyResolverCache.getOrPut(this) { PropertyResolverImpl(this) }
+    get() = PropertyResolverImpl(this)
