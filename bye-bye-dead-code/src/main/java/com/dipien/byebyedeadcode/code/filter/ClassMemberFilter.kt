48c7998dc8c84b6c8ec2218d7a8d3c114e50be61
Gonzalo <gonzalo.iac@gmail.com>
2021-05-01 19:32:55 -0300

    Fix logs

--- bye-bye-dead-code/src/main/java/com/dipien/byebyedeadcode/code/filter/ClassMemberFilter.kt
+++ bye-bye-dead-code/src/main/java/com/dipien/byebyedeadcode/code/filter/ClassMemberFilter.kt
@@ -3,7 +3,7 @@ package com.dipien.byebyedeadcode.code.filter
 import com.dipien.byebyedeadcode.code.DeadCode
 import com.dipien.byebyedeadcode.commons.LoggerHelper
 
-class ClassMemberFilter(private val regex : Regex, private val name: String) : DeadCodeFilter {
+class ClassMemberFilter(private val regex: Regex, private val name: String) : DeadCodeFilter {
 
     override fun filter(deadCode: DeadCode): DeadCode? {
         if (deadCode.classMembers.isEmpty()) {
@@ -11,19 +11,18 @@ class ClassMemberFilter(private val regex : Regex, private val name: String) : D
         }
 
         val membersToRemove = deadCode.classMembers.filter { it.matches(regex) }
+        if (membersToRemove.isNotEmpty()) {
+            LoggerHelper.info("[$name] Members ignored for class: ${deadCode.className}")
+            membersToRemove.forEach {
+                deadCode.classMembers.remove(it)
+                LoggerHelper.info("[$name] $it")
+            }
+        }
         return if (deadCode.classMembers.size == membersToRemove.size) {
             LoggerHelper.info("[$name] No members. Ignored: ${deadCode.className}")
             null
         } else {
-            if (membersToRemove.isNotEmpty()) {
-                LoggerHelper.info("[$name] Members ignored for class: ${deadCode.className}")
-                membersToRemove.forEach {
-                    deadCode.classMembers.remove(it)
-                    LoggerHelper.info("[$name] $it")
-                }
-            }
             deadCode
         }
     }
-
 }
