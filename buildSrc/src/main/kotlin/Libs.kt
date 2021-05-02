e1c25132dd8e9690a642896331aa0e784041cda3
dipien-ci <dipien2020@gmail.com>
2021-05-02 00:24:58 +0000

    Upgraded org.ow2.asm:asm from 9.0 to 9.1

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -6,7 +6,7 @@ object Libs {
     const val TRUTH = "com.google.truth:truth:1.0.1"
 
     // https://asm.ow2.io/versions.html
-    const val ASM = "org.ow2.asm:asm:9.0"
+    const val ASM = "org.ow2.asm:asm:9.1"
     const val ASM_UTILS = "org.ow2.asm:asm-util:9.0"
     const val ASM_COMMONS = "org.ow2.asm:asm-commons:9.0"
 }
