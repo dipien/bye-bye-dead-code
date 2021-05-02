1985a3a37d7e1dfee90bd390e9a368020fd3e83e
Gonzalo <gonzalo.iac@gmail.com>
2021-05-02 13:01:32 -0300

    Use literal syntax to make regular expressions easier to read

--- bye-bye-dead-code/src/main/java/com/dipien/byebyedeadcode/code/filter/CompilerCodeFilter.kt
+++ bye-bye-dead-code/src/main/java/com/dipien/byebyedeadcode/code/filter/CompilerCodeFilter.kt
@@ -7,21 +7,21 @@ class CompilerCodeFilter(filterContext: FilterContext) : DeadCodeFilter {
     companion object {
         // Class Filters
         // e.g: 'com.example.MyClass$1'
-        const val ANONYMOUS_CLASS = ".+\\\$\\d+\$"
+        const val ANONYMOUS_CLASS = """.+\$\d+$"""
         // e.g: 'com.example.MyInterface$DefaultImpls'
-        const val DEFAULT_IMPLES = ".+\\\$DefaultImpls\$"
+        const val DEFAULT_IMPLES = """.+\${'$'}DefaultImpls$"""
 
         // Member Filters
         // e.g: 'public final java.lang.String component1()'
-        const val COMPONENT_N_FUNCTION = ".+final.+component\\d+\\(\\)"
+        const val COMPONENT_N_FUNCTION = """.+final.+component\d+\(\)"""
         // e.g: 'public com.example.MyClass copy(java.lang.String)'
         // e.g: 'public final com.example.MyClass copy$default(java.lang.String)'
-        const val COPY_FUNCTION = ".+ copy(\\\$default)?\\(.+\\)"
+        const val COPY_FUNCTION = """.+ copy(\${'$'}default)?\(.+\)"""
         // e.g: 'public final com.example.MyClass getMyClass()'
         // e.g: 'public final boolean isEnabled()'
-        const val GET_FUNCTION = ".+ ([bB]oolean is|get).+\\(\\)"
+        const val GET_FUNCTION = """.+ ([bB]oolean is|get).+\(\)"""
         // e.g: 'public final void setMyClass(com.example.MyClass)'
-        const val SET_FUNCTION = ".+ set[A-Z].+\\(.+\\)"
+        const val SET_FUNCTION = """.+ set[A-Z].+\(.+\)"""
     }
 
     private val filters = listOf(
