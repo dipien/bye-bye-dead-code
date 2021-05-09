977b028b9868e681640d80efd38a495216147b22
Dipien CI <74492091+dipien-ci@users.noreply.github.com>
2021-05-09 08:24:37 -0300

    Upgraded com.google.truth:truth from 1.0.1 to 1.1.2 (#15)

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -3,7 +3,7 @@ object Libs {
     // https://github.com/junit-team/junit4/releases
     const val JUNIT = "junit:junit:4.13.2"
 
-    const val TRUTH = "com.google.truth:truth:1.0.1"
+    const val TRUTH = "com.google.truth:truth:1.1.2"
 
     // https://asm.ow2.io/versions.html
     const val ASM = "org.ow2.asm:asm:9.0"
