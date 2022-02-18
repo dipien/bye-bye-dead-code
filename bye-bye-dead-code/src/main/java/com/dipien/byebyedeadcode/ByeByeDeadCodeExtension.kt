afeb8fa6c098758fce1b585da4066d6185ed5975
Gonzalo Iacomuzzi <gonzalo.iac@gmail.com>
2022-02-18 14:41:15 -0300

    Change report file path (#41)
    * Change report file path
    
    * Save report file in build directory

--- bye-bye-dead-code/src/main/java/com/dipien/byebyedeadcode/ByeByeDeadCodeExtension.kt
+++ bye-bye-dead-code/src/main/java/com/dipien/byebyedeadcode/ByeByeDeadCodeExtension.kt
@@ -13,10 +13,10 @@ open class ByeByeDeadCodeExtension(propertyResolver: PropertyResolver) {
 
     var dryRun: Boolean = propertyResolver.getRequiredBooleanProp(::dryRun.name, false)
 
-    // Lists the code that was stripped by Proguard/R8
+    // Lists the code that was stripped by R8
     var proguardUsageFilePath = "app/build/outputs/mapping/release/usage.txt"
 
-    var reportFilePath = "android_dead_code.txt"
+    var reportFilePath = "app/build/outputs/dead_code/usage.txt"
 
     // Module dirs
     var compiledKotlinClassesDir = "build/tmp/kotlin-classes/release"
